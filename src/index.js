60b390440cc91dcac3c165f1c275d06705b3149f
yosu <woodstock830@gmail.com>
2020-09-07 16:58:24 +0900

    Make a GameStatus component.

--- src/index.js
+++ src/index.js
@@ -81,6 +81,22 @@ class History extends React.Component {
   }
 }
 
+class GameStatus extends React.Component {
+  render() {
+    const winner = calculateWinner(this.props.squares)
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.props.xIsNext ? 'X' : 'O');
+    }
+    return (
+      <div>{status}</div>
+    );
+  }
+}
+
 class Game extends React.Component {
   constructor(props) {
     super(props)
@@ -153,7 +169,10 @@ class Game extends React.Component {
           />
         </div>
         <div className="game-info">
-          <div>{status}</div>
+          <GameStatus
+            squares={current.squares}
+            xIsNext={this.state.xIsNext}
+          />
           <div className="history-order-toggle">
             <button onClick={() => this.handleHistoryOrderClick()}>{order}</button>
           </div>
@@ -199,23 +218,6 @@ function calculateWinner(squares) {
   const line = calculateWinnerLine(squares)
   if (line) {
     return squares[line[0]];
-  }
-  return null;
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
   }
   return null;
 }
